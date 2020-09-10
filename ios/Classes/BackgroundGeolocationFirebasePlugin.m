a792896a6b2d1be483de65aeea9d75667130aafc
Bahadir Aksoy <knissophiliac@gmail.com>
2020-09-10 18:34:55 +0300

    Improvement

--- ios/Classes/BackgroundGeolocationFirebasePlugin.m
+++ ios/Classes/BackgroundGeolocationFirebasePlugin.m
@@ -1,5 +1,6 @@
 #import "BackgroundGeolocationFirebasePlugin.h"
 
+#import <CoreLocation/CoreLocation.h>
 #import "Firebase.h"
 
 static NSString *const PLUGIN_PATH = @"com.transistorsoft/flutter_background_geolocation_firebase";
@@ -81,26 +82,100 @@ static NSString *const DEFAULT_GEOFENCES_COLLECTION = @"geofences";
 -(void) onPersist:(NSNotification*)notification {
     NSDictionary *data = notification.object;
     NSString *collectionName = (data[@"location"][@"geofence"]) ? _geofencesCollection : _locationsCollection;
+    NSArray *items = [collectionName componentsSeparatedByString:@"/"];
+    NSString *userId = [items objectAtIndex:1]; 
+    NSString *userDocumentPath = [NSString stringWithFormat:@"%@/%@", @"users", userId];
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-        FIRFirestore *db = [FIRFirestore firestore];
+        FIRDatabase *realtime = [FIRDatabase database];
+        
+        NSDictionary *dict = @{ @"locData" : @{
+                                    @"lat" : data[@"location"][@"coords"][@"latitude"],
+                                    @"lng" : data[@"location"][@"coords"][@"longitude"],
+                                    @"batteryLevel" : data[@"location"][@"battery"][@"level"]
+                                },
+                                @"locTime" : [FIRServerValue timestamp]
+        };
+        //FIRFirestore *db = [FIRFirestore firestore];
+        
         // Add a new document with a generated ID
         if (!self.updateSingleDocument) {
-            __block FIRDocumentReference *ref = [[db collectionWithPath:collectionName] addDocumentWithData:notification.object completion:^(NSError * _Nullable error) {
+            FIRDatabaseReference *realtimeRef = [realtime reference];
+            NSString *lastLatKey = @"flutter.last_lat";
+            NSString *lastLongKey = @"flutter.last_long";
+            NSString *appDomain = [[NSBundle mainBundle] bundleIdentifier];
+            NSDictionary *prefs = [[NSUserDefaults standardUserDefaults] persistentDomainForName:appDomain];
+
+            //Get old ones
+            NSNumber *lastLat;
+            if(prefs[lastLatKey] == nil){
+                lastLat = @0.0;
+            }else{
+                lastLat = prefs[lastLatKey];
+            }
+            NSNumber *lastLong;
+            if(prefs[lastLongKey] == nil){
+                lastLong = @0.0;
+            }else{
+                lastLong = prefs[lastLongKey];
+            }
+            NSNumber *newLat = data[@"location"][@"coords"][@"latitude"];
+            NSNumber *newLong = data[@"location"][@"coords"][@"longitude"];
+
+            CLLocation *oldLocation =  [[CLLocation alloc] initWithLatitude:lastLat.doubleValue longitude:lastLong.doubleValue];
+            CLLocation *newLocation =  [[CLLocation alloc] initWithLatitude:newLat.doubleValue longitude:newLong.doubleValue];
+
+            CLLocationDistance distanceInMeters = [oldLocation distanceFromLocation:newLocation];
+
+            NSNumber *minDistanceInMeters = @20.0;
+            
+            //if both are 0.0 or distance>20meter
+            if(([lastLat  isEqual: @0.0] && [lastLong  isEqual: @0.0]) || distanceInMeters > minDistanceInMeters.doubleValue){
+                //ancak ve ancak büyükse trackine ekle, ve eklediysen shared prefe yaz
+                [[[realtimeRef child:collectionName] childByAutoId] setValue:dict withCompletionBlock:^(NSError *error, FIRDatabaseReference *ref) {
+                    if (error) {
+                        NSLog(@"Data could not be saved: %@", error);
+                    } else {
+                        NSLog(@"Data saved successfully.");
+                        //TODO set last_lat last_long into shared prefs
+                        [[NSUserDefaults standardUserDefaults] setDouble:newLat.doubleValue forKey:lastLatKey];
+                        [[NSUserDefaults standardUserDefaults] setDouble:newLong.doubleValue forKey:lastLongKey];
+                        [[NSUserDefaults standardUserDefaults] synchronize];
+                    }
+                }];
+            }
+            
+            [[realtimeRef child:userDocumentPath] setValue:dict withCompletionBlock:^(NSError *error, FIRDatabaseReference *ref) {
+                if (error) {
+                    NSLog(@"Data could not be saved: %@", error);
+                } else {
+                    NSLog(@"Data saved successfully.");
+                }
+            }];
+
+
+            /* __block FIRDocumentReference *ref = [[db collectionWithPath:collectionName] addDocumentWithData:dict completion:^(NSError * _Nullable error) {
                 if (error != nil) {
                     NSLog(@"Error adding document: %@", error);
                 } else {
                     NSLog(@"Document added with ID: %@", ref.documentID);
                 }
             }];
-        } else {
-            [[db documentWithPath:collectionName] setData:notification.object completion:^(NSError * _Nullable error) {
+            [[db documentWithPath:userDocumentPath] setData:dict merge:YES completion:^(NSError * _Nullable error) {
                 if (error != nil) {
                     NSLog(@"Error writing document: %@", error);
                 } else {
                     NSLog(@"Document successfully written");
                 }
-            }];
+            }]; */
+        } else {
+            /* [[db documentWithPath:collectionName] setData:notification.object completion:^(NSError * _Nullable error) {
+                if (error != nil) {
+                    NSLog(@"Error writing document: %@", error);
+                } else {
+                    NSLog(@"Document successfully written");
+                }
+            }]; */
         }
     });
 }
